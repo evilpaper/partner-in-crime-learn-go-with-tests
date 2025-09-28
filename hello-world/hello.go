767b4070d151e92e971498d770cd4f489d6ab413
Pelle Lundgren <pellburt@gmail.com>
2025-09-28 20:26:17 +0200

    refactor: separate domain code from outside world (side-effects)

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// This means this function returns a string.
+func Hello() string {
+	return "Hello, World!"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(Hello())
 }
