fb222f2d1951082b9c45aacb30cc0ea0e6ef6261
Pelle Lundgren <pellburt@gmail.com>
2025-09-29 20:37:52 +0200

    refactor: wrap test in a subtest for better readability

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,16 +4,19 @@ import "testing"
 
 // t of type *testing.T is our "hook" into the testing framework
 func TestHello(t *testing.T) {
-	// Declaring variables
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	// Calling the Errorf method on our t, which will print 
-	// out a message and fail the test. The f stands for format,
-	// which allows us to build a string with values inserted into 
-	// the placeholder values %q. When you make the test fail, 
-	// it should be clear how it works.
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		// Declaring variables
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		// Calling the Errorf method on our t, which will print 
+		// out a message and fail the test. The f stands for format,
+		// which allows us to build a string with values inserted into 
+		// the placeholder values %q. When you make the test fail, 
+		// it should be clear how it works.
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
