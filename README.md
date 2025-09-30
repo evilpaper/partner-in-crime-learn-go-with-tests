846af8197d31d46a7b681216164c5f2a61e0ed31
Pelle Lundgren <pellburt@gmail.com>
2025-09-30 07:26:49 +0200

    refactor: refactor assertion into a new function to reduces duplication and improves readability

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Companion repo for exploring Go with [Learn Go with Tests](https://quii.gitbook.
 
 Open the whole folder in your IDE of choice.
 
-Hack along
+Hack along!
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -2,29 +2,23 @@ package main
 
 import "testing"
 
-// t of type *testing.T is our "hook" into the testing framework
 func TestHello(t *testing.T) {
-
 	t.Run("saying hello to people", func(t *testing.T) {
-		// Declaring variables
+	
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		// Calling the Errorf method on our t, which will print 
-		// out a message and fail the test. The f stands for format,
-		// which allows us to build a string with values inserted into 
-		// the placeholder values %q. When you make the test fail, 
-		// it should be clear how it works.
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
