fbecdc1282a6e1d80359d44a5ec842956febff1a
Pelle Lundgren <pellburt@gmail.com>
2025-10-01 18:45:01 +0200

    refactor(GREEN): pass test by adding swedish to languages

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
-const french = "French"
+const french = "French"	
+const swedish = "Swedish"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
+const swedishHelloPrefix = "Hej, "
 
 // This means this function accept a string and returns a string.
 func Hello(name string, language string) string {
@@ -22,6 +24,8 @@ func Hello(name string, language string) string {
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	case swedish:
+		prefix = swedishHelloPrefix
 	}
 
 	return prefix + name	
