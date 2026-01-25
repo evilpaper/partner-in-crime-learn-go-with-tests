0b99ec96a2c0c559eb320f0b39f944263ff0116a
Pelle Lundgren <pellburt@gmail.com>
2026-01-25 22:33:45 +0100

    refactor(GREEN): pass second parameter test by adding it to Repeat func and use it inside func loop

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, count int) string {
 	result := ""
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		result += character
 	} 
 	return result
