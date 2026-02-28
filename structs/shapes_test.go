593b80eeb3b83a6c396b78ef67f9b367b54f82d0
Pelle Lundgren <pellburt@gmail.com>
2026-02-28 09:00:10 +0100

    test(RED): add test for area of a circle

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,19 +5,33 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
-	expected := 40.0
+	want := 40.0
 
-	if got != expected {
-		t.Errorf("got %.2f expected %.2f", got, expected)
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	expected := 100.0
 
-	if got != expected {
-		t.Errorf("got %.2f expected %.2f", got, expected)
-	}	
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}	
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+		
+	})
 }
