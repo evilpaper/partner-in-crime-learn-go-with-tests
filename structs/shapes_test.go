5bc83536290c8c449acbe47c85a7d169d9dd20a0
Pelle Lundgren <pellburt@gmail.com>
2026-03-03 19:22:13 +0100

    refactor: add checkArea to test to avoid duplication

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -12,26 +12,27 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func TestArea(t *testing.T) {
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
 
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}	
+		checkArea(t, rectangle, 72.0)	
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-		
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
