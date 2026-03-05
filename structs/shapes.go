9c241938cdcb17dc927fe5e99bb97a2a935fa6fd
Pelle Lundgren <pellburt@gmail.com>
2026-03-05 23:01:24 +0100

    refactor: define Triangle stuct

--- structs/shapes.go
+++ structs/shapes.go
@@ -19,6 +19,11 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
