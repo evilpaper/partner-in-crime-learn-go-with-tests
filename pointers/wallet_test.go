fde1ecaa4b70a1e5b9a9316b8e647ee4c4b244a8
Pelle Lundgren <pellburt@gmail.com>
2026-03-10 19:09:50 +0100

    test(RED): add test for pointer

---
+++ pointers/wallet_test.go
@@ -0,0 +1,13 @@
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10 
+
+	if got !=want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
