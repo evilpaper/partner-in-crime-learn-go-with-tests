be2c7fce093c9102521e196626cf24f58bcc2943
Pelle Lundgren <pellburt@gmail.com>
2026-04-03 07:19:30 +0200

    docs: add comment about no need to dereference the pointer

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -11,6 +11,7 @@ type Wallet struct {
 // Notice the start (*). Read it as "a pointer to a Wallet"
 func (wallet *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %p \n", &wallet.balance)
+	// We don't need to dereference the pointer here because we are working with a struct. Just how the language works.
 	wallet.balance += amount
 }
 
