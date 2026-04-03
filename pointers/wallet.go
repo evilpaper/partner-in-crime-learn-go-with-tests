6aa746bd46c6db95ea7349cbd52c9312b7c30cc1
Pelle Lundgren <pellburt@gmail.com>
2026-04-03 07:33:12 +0200

    refactor: clean up logs

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,17 +1,12 @@
 package pointers
 
-import (
-	"fmt"
-)
-
 type Wallet struct {
 	balance int
 }
 
-// Notice the start (*). Read it as "a pointer to a Wallet"
+// Note the star (*), it means "a pointer to a Wallet"
 func (wallet *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %p \n", &wallet.balance)
-	// We don't need to dereference the pointer here because we are working with a struct. Just how the language works.
+	// Note that we don't need to dereference the pointer, it works automatically. Just how the language works.
 	wallet.balance += amount
 }
 
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,7 +1,6 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -12,8 +11,6 @@ func TestWallet(t *testing.T) {
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
-
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
 	want := 10 
 
