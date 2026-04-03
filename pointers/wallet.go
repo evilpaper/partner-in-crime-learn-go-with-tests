ee4f19947f9d357784d0fdfa7d2ccf47995e26f5
Pelle Lundgren <pellburt@gmail.com>
2026-04-03 07:35:58 +0200

    refactor: replace int with custom Bitcoin type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,15 +1,17 @@
 package pointers
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Note the star (*), it means "a pointer to a Wallet"
-func (wallet *Wallet) Deposit(amount int) {
+func (wallet *Wallet) Deposit(amount Bitcoin) {
 	// Note that we don't need to dereference the pointer, it works automatically. Just how the language works.
 	wallet.balance += amount
 }
 
-func (wallet *Wallet) Balance() int {
+func (wallet *Wallet) Balance() Bitcoin {
 	return wallet.balance
 }
