5e2548edb4c02d4e2b53a6b3109b79dad0a4b48f
Pelle Lundgren <pellburt@gmail.com>
2026-04-03 07:16:09 +0200

    refactor(GREEN): point to a fixed wallet instead of making copies each time we call functions on them

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -8,11 +8,12 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
-	w.balance += amount
+// Notice the start (*). Read it as "a pointer to a Wallet"
+func (wallet *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %p \n", &wallet.balance)
+	wallet.balance += amount
 }
 
-func (w Wallet) Balance() int {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
-	return w.balance
+func (wallet *Wallet) Balance() int {
+	return wallet.balance
 }
