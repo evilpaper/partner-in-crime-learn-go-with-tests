de49f5944c41c32e269ffd1999444f895b921221
Pelle Lundgren <pellburt@gmail.com>
2026-06-20 09:41:24 +0200

    refactor(pointers): extract assertBalance function in test, remove duplicated code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,18 +6,19 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10) 
-
-		if got !=want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
@@ -25,12 +26,6 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
