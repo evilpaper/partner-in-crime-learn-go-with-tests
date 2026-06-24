80e8da0898a38ea026540a50fe72c7bbc98bc457
Pelle Lundgren <pellburt@gmail.com>
2026-06-24 10:42:56 +0200

    docs(pointers): explain rationale behind creating a Bitcoin type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -2,9 +2,14 @@ package pointers
 
 import "errors"
 
+// We want our wallet to contain Bitcoins. We could use just raw int but we want our wallet to know the concept of a Bitcoin.
+// So we create a new type called Bitcoin that is an int.
+// int is good for counting things.
+// Go lets us create new types from existing types. So here we have created a new type called Bitcoin that is an int.
 type Bitcoin int
 
 type Wallet struct {
+	// Note the lowercase balance. In Go, lowercase means it's private. That is not exported to other packages.
 	balance Bitcoin
 }
 
